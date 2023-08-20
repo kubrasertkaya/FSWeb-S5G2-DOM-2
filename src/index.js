404150bbf406b557c6e3a1bfbbce6fed60088052
kubrasertkaya <118736856+kubrasertkaya@users.noreply.github.com>
2023-08-20 23:50:48 +0300

    proje tamamlandı.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import e from 'cors';
 import './less/index.less'
 
 // Örnek bir event kullanımı aşağıdadır. Çalıştırmak için comment dışına alın
@@ -7,3 +8,52 @@ import './less/index.less'
 
 
 // Kodlar buraya gelecek!
+document.querySelector("img").addEventListener("wheel", function (e) {
+    e.preventDefault();
+    e.target.src = "https://picsum.photos/1000/300";
+    setTimeout(() => {
+        e.target.src = "http://localhost:9000/img/fun-bus.jpg";
+    }, 1000);
+});
+
+const someH1 = document.querySelector("h1");
+function mouseHover(domElem) {
+    domElem.addEventListener("mouseover", (e) => {
+        e.target.style.transform = `scale(1.5)`;
+    });
+    domElem.addEventListener("mouseout", (e) => {
+        e.target.style.transform = `scale(1)`;
+    });
+
+}
+mouseHover(someH1);
+const someButton = document.querySelector(".btn");
+mouseHover(someButton);
+
+const navs = document.querySelectorAll("nav a")
+// for(const nav of navs){
+// nav.addEventListener("mouseover",(e)=>{
+//   e.target.style.animation="direction"}
+// )
+// nav.addEventListener("mouseleave",(e)=>{
+//     e.target.style.animation=""}
+// )
+// };
+for (let i in navs) {
+    navs[i].addEventListener("mouseover", (e) => {
+        navs[i].style.color = "pink"
+    }
+    )
+    navs[i].addEventListener("mouseleave", (e) => {
+        navs[i].style.color = "black"
+    }
+    )
+};
+const myDIV = document.createElement("div");
+const parentElement = document.getElementsByClassName("content-pick");
+parentElement.appendChild(myDIV);
+
+function myFunction() {
+    document.getElementById("myDIV").style.animation = "mynewmove 4s 2";
+  }
+myFunction(myDIV);
